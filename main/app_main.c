a9ccc1058342862ff5ac9977b32b75b2afa209c4
Renato Freitas <freitas-renato@outlook.com>
2020-10-19 14:14:20 -0300

    :lock: Require mqtt credentials

--- main/app_main.c
+++ main/app_main.c
@@ -86,6 +86,8 @@ static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_
 static void mqtt_app_start(void *arg) {
     esp_mqtt_client_config_t mqtt_cfg = {
         .uri = CONFIG_BROKER_URL,
+        .username = CONFIG_BROKER_USERNAME,
+        .password = CONFIG_BROKER_PASSWORD
     };
 
     esp_mqtt_client_handle_t client = esp_mqtt_client_init(&mqtt_cfg);
